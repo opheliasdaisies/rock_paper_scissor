2327151a8e39888685f46d2a7ae6d8729dbb5a7c
Sara Gorecki <sgorecki@gmail.com>
2013-12-10 11:43:06 -0500

    add routes

--- game.rb
+++ game.rb
@@ -3,6 +3,14 @@ Bundler.require
 
 module Game
   class RPS_App < Sinatra::Application
+    
+    get "/throw" do
+      erb :throw
+    end
+
+    get "/throw/:move" do
+      @move = params[:type]
+    end
 
   end
 end
