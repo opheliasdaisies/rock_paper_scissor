3499f7f73f9cdcab76d7a009e0739ecefeee8e52
Sara Gorecki <sgorecki@gmail.com>
2013-12-10 13:27:44 -0500

    add :move param back in to route

--- game.rb
+++ game.rb
@@ -12,9 +12,9 @@ module Game
       erb :throw
     end
 
-    get "/throw/rock" do
-      game.player.move = "rock"
-      game.computer.rand_move
+    get "/throw/:move" do
+      @player_move = game.player.move = params[:move].to_s
+      @computer_move = game.computer.rand_move
       @message = game.win_or_lose
 
       erb :move
